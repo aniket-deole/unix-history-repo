aniket-deole/unix-history-repo

commit b8f7ca981fabd55a0df22f1dd7aa6d76b5e00976

- Only count the number of CPUs in the rendezvous map once rather than   doing it on every CPU. - Use CPU_ABSENT() rather than pcpu_find() to determine if a CPU is not   present. - Count up to mp_maxid rather than MAXCPU when iterating over CPUs to   match the rest of the code in the kernel.
MFC after:	1 week


git-svn-id: svn+ssh://svn.freebsd.org/base/head@182292 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/kern/subr_smp.c
@@ -111,7 +111,7 @@ SYSCTL_INT(_kern_smp, OID_AUTO, forward_roundrobin_enabled, CTLFLAG_RW,
 	   "Forwarding of roundrobin to all other CPUs");
 
 /* Variables needed for SMP rendezvous. */
-static volatile cpumask_t smp_rv_cpumask;
+static volatile int smp_rv_ncpus;
 static void (*volatile smp_rv_setup_func)(void *arg);
 static void (*volatile smp_rv_action_func)(void *arg);
 static void (*volatile smp_rv_teardown_func)(void *arg);
@@ -306,20 +306,14 @@ restart_cpus(cpumask_t map)
 void
 smp_rendezvous_action(void)
 {
-	cpumask_t map = smp_rv_cpumask;
-	int i, ncpus = 0;
 	void* local_func_arg = smp_rv_func_arg;
 	void (*local_setup_func)(void*)   = smp_rv_setup_func;
 	void (*local_action_func)(void*)   = smp_rv_action_func;
 	void (*local_teardown_func)(void*) = smp_rv_teardown_func;
 
-	for (i = 0; i < MAXCPU; i++)
-		if (((1 << i) & map) != 0 && pcpu_find(i) != NULL)
-			ncpus++;
-
 	/* Ensure we have up-to-date values. */
 	atomic_add_acq_int(&smp_rv_waiters[0], 1);
-	while (smp_rv_waiters[0] < ncpus)
+	while (smp_rv_waiters[0] < smp_rv_ncpus)
 		cpu_spinwait();
 
 	/* setup function */
@@ -329,7 +323,7 @@ smp_rendezvous_action(void)
 
 		/* spin on entry rendezvous */
 		atomic_add_int(&smp_rv_waiters[1], 1);
-		while (smp_rv_waiters[1] < ncpus)
+		while (smp_rv_waiters[1] < smp_rv_ncpus)
                 	cpu_spinwait();
 	}
 
@@ -341,7 +335,7 @@ smp_rendezvous_action(void)
 	atomic_add_int(&smp_rv_waiters[2], 1);
 	if (local_teardown_func == smp_no_rendevous_barrier)
                 return;
-	while (smp_rv_waiters[2] < ncpus)
+	while (smp_rv_waiters[2] < smp_rv_ncpus)
 		cpu_spinwait();
 
 	/* teardown function */
@@ -368,15 +362,15 @@ smp_rendezvous_cpus(cpumask_t map,
 		return;
 	}
 
-	for (i = 0; i < MAXCPU; i++)
-		if (((1 << i) & map) != 0 && pcpu_find(i) != NULL)
+	for (i = 0; i < mp_maxid; i++)
+		if (((1 << i) & map) != 0 && !CPU_ABSENT(i))
 			ncpus++;
-		
+
 	/* obtain rendezvous lock */
 	mtx_lock_spin(&smp_ipi_mtx);
 
 	/* set static function pointers */
-	smp_rv_cpumask = map;
+	smp_rv_ncpus = ncpus;
 	smp_rv_setup_func = setup_func;
 	smp_rv_action_func = action_func;
 	smp_rv_teardown_func = teardown_func;

commit 00ec40dcc816de2928b8fe70c04c7294c0041c20

Fix range-check error introduced in r182292. Also do not do anything if all processors in the map are not available, simply return.
Approved by:	kib (mentor)
MFC after:	1 week

sys/kern/subr_smp.c
@@ -362,9 +362,11 @@ smp_rendezvous_cpus(cpumask_t map,
 		return;
 	}
 
-	for (i = 0; i < mp_maxid; i++)
+	for (i = 0; i <= mp_maxid; i++)
 		if (((1 << i) & map) != 0 && !CPU_ABSENT(i))
 			ncpus++;
+	if (ncpus == 0)
+		return;
 
 	/* obtain rendezvous lock */
 	mtx_lock_spin(&smp_ipi_mtx);