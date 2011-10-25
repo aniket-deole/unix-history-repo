aniket-deole/unix-history-repo

commit dc993caadb6453a90c1b2947afb16232596d8b68

Update copyright information in several ZFS files, as the clause 3.3 of the CDDL licence explicitly requires every Contributor to add a copyright notice.
This also reflects the copyright notices for the changes recently
added by Illumos.

MFC after:	3 days


git-svn-id: svn+ssh://svn.freebsd.org/base/head@226724 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_znode.c
@@ -23,6 +23,7 @@
  */
 
 /* Portions Copyright 2007 Jeremy Teo */
+/* Portions Copyright 2011 Martin Matuska <mm@FreeBSD.org */
 
 #ifdef _KERNEL
 #include <sys/types.h>

sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sa.c
@@ -20,6 +20,7 @@
  */
 /*
  * Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
+ * Portions Copyright 2011 iXsystems, Inc
  */
 
 #include <sys/zfs_context.h>

sys/cddl/contrib/opensolaris/uts/common/fs/zfs/txg.c
@@ -20,6 +20,7 @@
  */
 /*
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Portions Copyright 2011 Martin Matuska <mm@FreeBSD.org>
  */
 
 #include <sys/zfs_context.h>

sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_ioctl.c
@@ -22,6 +22,7 @@
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2011 Pawel Jakub Dawidek <pawel@dawidek.net>.
  * All rights reserved.
+ * Portions Copyright 2011 Martin Matuska <mm@FreeBSD.org>
  */
 
 #include <sys/types.h>

sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zvol.c
@@ -26,6 +26,7 @@
  */
 
 /* Portions Copyright 2010 Robert Milkowski */
+/* Portions Copyright 2011 Martin Matuska <mm@FreeBSD.org> */
 
 /*
  * ZFS volume emulation driver.

commit 05eda6a6b587037ecb0e2e26bc2279d6aa102ca4

Fix typo in copyright notice introduced in r226724 (missing character in e-mail adress)
Reported by:	pjd
MFC after:	3 days

sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_znode.c
@@ -23,7 +23,7 @@
  */
 
 /* Portions Copyright 2007 Jeremy Teo */
-/* Portions Copyright 2011 Martin Matuska <mm@FreeBSD.org */
+/* Portions Copyright 2011 Martin Matuska <mm@FreeBSD.org> */
 
 #ifdef _KERNEL
 #include <sys/types.h>