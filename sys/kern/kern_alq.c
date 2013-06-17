aniket-deole/unix-history-repo

commit 12815294499eb9ec25f6e8c0aa07d7cdb7957c54

Ensure alq's shutdown_pre_sync event handler is deregistered on module unload to avoid a dangling pointer and eventual panic on system shutdown.
Reported by:	Ali <comnetboy at gmail.com>
Tested by:	Ali <comnetboy at gmail.com>
MFC after:	1 week


git-svn-id: svn+ssh://svn.freebsd.org/base/head@250951 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/kern/kern_alq.c
@@ -99,6 +99,7 @@ static LIST_HEAD(, alq) ald_active;
 static int ald_shutingdown = 0;
 struct thread *ald_thread;
 static struct proc *ald_proc;
+static eventhandler_tag alq_eventhandler_tag = NULL;
 
 #define	ALD_LOCK()	mtx_lock(&ald_mtx)
 #define	ALD_UNLOCK()	mtx_unlock(&ald_mtx)
@@ -194,8 +195,8 @@ ald_daemon(void)
 
 	ald_thread = FIRST_THREAD_IN_PROC(ald_proc);
 
-	EVENTHANDLER_REGISTER(shutdown_pre_sync, ald_shutdown, NULL,
-	    SHUTDOWN_PRI_FIRST);
+	alq_eventhandler_tag = EVENTHANDLER_REGISTER(shutdown_pre_sync,
+	    ald_shutdown, NULL, SHUTDOWN_PRI_FIRST);
 
 	ALD_LOCK();
 
@@ -227,6 +228,8 @@ static void
 ald_shutdown(void *arg, int howto)
 {
 	struct alq *alq;
+
+	EVENTHANDLER_DEREGISTER(shutdown_pre_sync, alq_eventhandler_tag);
 
 	ALD_LOCK();
 

commit 0743c3f96d4f81ec2bfda8b8b0d9efec49994205

The fix committed in r250951 replaced the reported panic with a deadlock... gold star for me. EVENTHANDLER_DEREGISTER() attempts to acquire the lock which is held by the event handler framework while executing event handler functions, leading to deadlock.
Move EVENTHANDLER_DEREGISTER() to alq_load_handler() and thus deregister the ALQ
shutdown_pre_sync handler at module unload time, which takes care of the
originally reported panic and fixes the deadlock introduced in r250951.

Reported by:	Luiz Otavio O Souza
MFC after:	3 days
X-MFC with:	250951


git-svn-id: svn+ssh://svn.freebsd.org/base/head@251838 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/kern/kern_alq.c
@@ -229,8 +229,6 @@ ald_shutdown(void *arg, int howto)
 {
 	struct alq *alq;
 
-	EVENTHANDLER_DEREGISTER(shutdown_pre_sync, alq_eventhandler_tag);
-
 	ALD_LOCK();
 
 	/* Ensure no new queues can be created. */
@@ -938,6 +936,8 @@ alq_load_handler(module_t mod, int what, void *arg)
 		if (LIST_FIRST(&ald_queues) == NULL) {
 			ald_shutingdown = 1;
 			ALD_UNLOCK();
+			EVENTHANDLER_DEREGISTER(shutdown_pre_sync,
+			    alq_eventhandler_tag);
 			ald_shutdown(NULL, 0);
 			mtx_destroy(&ald_mtx);
 		} else {